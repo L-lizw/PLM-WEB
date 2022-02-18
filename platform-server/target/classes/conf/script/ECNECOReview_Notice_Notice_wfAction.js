d856b9302cac76d200604ecc288eb26261e10284
L-lizw <87376684+L-lizw@users.noreply.github.com>
2022-02-18 11:08:08 +0800

    Delete platform-server/target/classes directory
    去除编译目录

--- platform-server/target/classes/conf/script/ECNECOReview_Notice_Notice_wfAction.js
+++
@@ -1,8 +0,0 @@
-var uesc= provider.getServiceInstance(Packages.dyna.net.service.brs.UECS.class, sid);
-var processGuid = inputObject.getProcessGuid();
-if(processGuid!=null || processGuid!="")
-{
-         print("start...\n");
-         uesc.getUECSStub().sendMailtoPerformerByScript(processGuid);
-         print("finished...\n");
-}
